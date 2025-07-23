a8e26e9206f16ee1f1d11c9dfa4ba8303464657a
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2025-07-23 14:48:24 -0600

    Squashed commit of the following:
    commit bdecadb880d6ee5546808ee242916b87f0d665f7
    Author: Guennadi Maximov C <g.maxc.fox@protonmail.com>
    Date:   Wed Jul 23 14:41:41 2025 -0600
    
        fix: Wanaged to define most, if not all, data type annotations.
    
        Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>
    
    commit 950bac10fb7d0033a3ce88bcb060cd109b6ddd24
    Author: Guennadi Maximov C <g.maxc.fox@protonmail.com>
    Date:   Tue Jul 22 20:26:04 2025 -0600
    
        feat(wezterm): Lots of documentation for `types.wezterm` (READ DESCRIPTION).
    
        - Added `wezterm.plugin` annotations
        - Added `wezterm.serde` annotations
        - Added `wezterm.url` annotations
        - Massively enhanced existing annotations
        - Added and/or re-positioned field descriptions
    
        Aditional commit(s):
        - fix(config): Added KDE option field and other field descriptions.
    
        Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>
    
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- lua/types/config/init.lua
+++ lua/types/config/init.lua
@@ -5,12 +5,19 @@
 ---@module "types.objects"
 ---@module "types.wezterm"
 
+---@alias IntStr integer|string
+---@alias NumStr number|string
+
 ---@alias DroppedFileQuoting
 ---|"None"
 ---|"SpacesOnly"
 ---|"Posix"
 ---|"Windows"
 ---|"WindowsAlwaysQuoted"
+
+---@alias ImePreeditRendering
+---|"Builtin"
+---|"System"
 
 ---@alias DefaultCursorStyle
 ---|"SteadyBlock"
@@ -25,11 +32,22 @@
 ---@field saturation? number
 ---@field brightness? number
 
+---@alias SystemBackdrop
+---|"Auto"
+---|"Disable"
+---|"Acrylic"
+---|"Mica"
+---|"Tabbed"
+
 ---@class WindowPadding
----@field left? integer|string
----@field right? integer|string
----@field top? integer|string
----@field bottom? integer|string
+---@field left? IntStr
+---@field right? IntStr
+---@field top? IntStr
+---@field bottom? IntStr
+
+---@alias WindowCloseConfirmation
+---|"AlwaysPrompt"
+---|"NeverPrompt"
 
 ---@class DaemonOptions
 ---@field stdout? string
@@ -43,21 +61,47 @@
 ---@field fade_out_function? EasingFunction
 ---@field target? "BackgroundColor"|"CursorColor"
 
+---@class BackgroundLayer.Style1
+---@field File string
+---@field speed? number
+
+---@class BackgroundLayer.Style2
+---@field Gradient Gradient
+---@field Color string
+
 ---@class BackgroundLayer
----@field style? { ["File"]: string, ["speed"]: number? }|{ ["Gradient"]: Gradient }|{ ["Color"]: string }
+---@field style? BackgroundLayer.Style1|BackgroundLayer.Style2
 ---@field attachment? "Fixed"|"Scroll"|{ ["Parallax"]: number }
 ---@field repeat_x? "Repeat"|"Mirror"|"NoRepeat"
----@field repeat_x_size? number|string
+---@field repeat_x_size? NumStr
 ---@field repeat_y? "Repeat"|"Mirror"|"NoRepeat"
----@field repeat_y_size? number|string
+---@field repeat_y_size? NumStr
 ---@field vertical_align? "Top"|"Middle"|"Bottom"
----@field vertical_offset? number|string
+---@field vertical_offset? NumStr
 ---@field horizontal_align? "Left"|"Center"|"Right"
----@field horizontal_offset? number|string
+---@field horizontal_offset? NumStr
 ---@field opacity? number
 ---@field hsb? HsbTransform
 ---@field height? "Cover"|"Contain"|number|string
 ---@field widtht? "Cover"|"Contain"|number|string
+
+---@alias NewlineCanon
+---|bool
+---|"None"
+---|"LineFeed"
+---|"CarriageReturn"
+---|"CarriageReturnAndLineFeed"
+
+---@alias KeyMapPreference
+---|"Mapped"
+---|"Physical"
+
+---@alias UIKeyCapRendering
+---|"UnixLong" Super, Meta, Ctrl, Shift.
+---|"Emacs" Super, M, C, S.
+---|"AppleSymbols" use macOS style symbols for Command, Option and so on
+---|"WindowsLong" Win, Alt, Ctrl, Shift
+---|"WindowsSymbols" like WindowsLong but using a logo for the Win key
 
 ---@class Config
 -- When combined with `window_background_opacity`, enables background blur
@@ -87,7 +131,7 @@
 ---@field color_scheme? String
 -- Use a named color scheme rather than the palette specified
 -- by the colors setting.
----@field color_schemes? {[String]: Palette}
+---@field color_schemes? { [String]: Palette }
 -- Named color schemes
 ---@field scrollback_lines? usize
 -- How many lines of scrollback you want to retain
@@ -224,7 +268,7 @@
 ---@field disable_default_quick_select_patterns? bool
 ---@field quick_select_patterns? String[]
 ---@field quick_select_alphabet? String
----@field mouse_bindings? Mouse[]
+---@field mouse_bindings? MouseBindingBase[]
 ---@field disable_default_mouse_bindings? bool
 ---@field daemon_options? DaemonOptions
 ---@field send_composed_key_when_left_alt_is_pressed? bool
@@ -415,8 +459,6 @@
 ---@field alternate_buffer_wheel_scroll_speed? u8
 ---@field status_update_interval? u64
 ---@field experimental_pixel_positioning? bool
----@field bidi_enabled? bool
----@field bidi_direction? ParagraphDirectionHint
 ---@field skip_close_confirmation_for_processes_named? String[]
 ---@field quit_when_all_windows_are_closed? bool
 ---@field warn_about_missing_glyphs? bool
