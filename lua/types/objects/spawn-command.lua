44480d6623b19a96c704fef962493e1751e89255
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2025-07-25 16:16:50 -0600

    Squashed commit of the following:
    commit 925d8fd03c9ca4d7c0b5f49988db39eff80cd888
    Author: Guennadi Maximov C <g.maxc.fox@protonmail.com>
    Date:   Fri Jul 25 12:20:23 2025 -0600
    
        docs: Annotated `Window`, 'MuxTabObj' and `SpawnCommand`.
    
        - fix(objects): Completed `SpawnCommand`.
        - fix: Annotated `Window` and `MuxTabObj` objects.
    
        Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>
    
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- lua/types/objects/spawn-command.lua
+++ lua/types/objects/spawn-command.lua
@@ -2,6 +2,26 @@
 
 --TODO: finish
 
+---@alias SpawnDomain
+---|"DefaultDomain"
+---|"CurrentPaneDomain"
+---|{ DomainName: string }
+
+---@alias SpawnPosition.Origin
+---|"ScreenCoordinateSystem"
+---|"MainScreen"
+---|"ActiveScreen"
+---|{ Named: string }
+
+---@class SpawnPosition
+---@field x? integer
+---@field y? integer
+---@field origin? SpawnPosition.Origin
+
 ---@class SpawnCommand
-local SpawnCommand = {}
-SpawnCommand.foo = "bar"
+---@field args string[]
+---@field label? string
+---@field cwd? string
+---@field set_environment_variables? table<string, any>|table
+---@field domain? "DefaultDomain"
+---@field position? SpawnPosition
