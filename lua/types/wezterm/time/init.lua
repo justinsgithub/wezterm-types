d3a846238bdf147da6c8655dd273c142fde5ff82
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2025-07-28 02:06:49 -0600

    fix(objects): Fixed `Time` object, descriptions added.
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- lua/types/wezterm/time/init.lua
+++ lua/types/wezterm/time/init.lua
@@ -4,10 +4,10 @@
 ---@class Wezterm.Time
 --  Arranges to call your callback function after the specified number of seconds have elapsed
 ---@field call_after fun(interval: number, function: function): nil
---  Returns a WezTermTimeObj object representing the time at which `wezterm.time.now()` was called
----@field now fun(): TimeObj
+--  Returns a WezTermTime object representing the time at which `wezterm.time.now()` was called
+---@field now fun(): Time
 --  Parses a string that is formatted according to the supplied format string
----@field parse fun(string): TimeObj
+---@field parse fun(string): Time
 --  Parses a string that is formatted according to `RFC 3339`
 --  and returns a Time object representing that time
----@field parse_rfc3339 fun(string): TimeObj
+---@field parse_rfc3339 fun(string): Time
