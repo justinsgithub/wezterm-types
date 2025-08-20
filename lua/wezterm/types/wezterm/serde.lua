357ee54211b6659441854d62ad524bdc9daabc04
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2025-08-20 17:03:29 -0600

    refactor(serde): Rewrote definitions for `Wezterm.Serde` class.
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- lua/wezterm/types/wezterm/serde.lua
+++ lua/wezterm/types/wezterm/serde.lua
@@ -1,18 +1,50 @@
 ---@meta
 
 ---The `wezterm.serde` module provides functions for parsing the given string as
----JSON, YAML, or TOML, returning the corresponding Lua values, and vice versa
+---JSON, YAML, or TOML, returning the corresponding Lua values, and vice versa.
+---
 ---@class Wezterm.Serde
----Parses the supplied string as JSON and returns the equivalent lua values
----@field json_decode fun(value: string): table
----Encodes the supplied lua value as JSON
----@field json_encode fun(value: table): string
----Encodes the supplied lua value as a pretty-printed string of JSON
----@field json_encode_pretty fun(value: table): string
----Parses the supplied string as TOML and returns the equivalent lua values
----@field toml_decode fun(value: string): table
----Encodes the supplied lua value as TOML
----@field toml_encode fun(value: table): string
----@field toml_encode_pretty fun(value: table): string
----@field yaml_decode fun(value: string): table
----@field yaml_encode fun(value: table): string
+local Serde = {}
+
+---Parses the supplied string as JSON and returns the equivalent Lua values.
+---
+---@param value string
+---@return table
+function Serde.json_decode(value) end
+
+---Encodes the supplied Lua value as JSON.
+---
+---@param value table
+---@return string
+function Serde.json_encode(value) end
+
+---Encodes the supplied Lua value as a pretty-printed string of JSON.
+---
+---@param value table
+---@return string
+function Serde.json_encode_pretty(value) end
+---Parses the supplied string as TOML and returns the equivalent Lua values.
+---
+---@param value string
+---@return table
+function Serde.toml_decode(value) end
+---Encodes the supplied Lua value as TOML.
+---
+---@param value table
+---@return string
+function Serde.toml_encode(value) end
+---Encodes the supplied Lua value as a pretty-printed string of TOML.
+---
+---@param value table
+---@return string
+function Serde.toml_encode_pretty(value) end
+---Parses the supplied string as YAML and returns the equivalent Lua values.
+---
+---@param value string
+---@return table
+function Serde.yaml_decode(value) end
+---Encodes the supplied Lua value as YAML.
+---
+---@param value table
+---@return string
+function Serde.yaml_encode(value) end
